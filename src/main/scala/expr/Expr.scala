f21bfa61870ea34e4a58b6bea224f57966f7436d
Andrey Gnatyuk <to.gnatuk@gmail.com>
2018-03-27 00:59:16 +0300

    Parser finished

--- src/main/scala/expr/Expr.scala
+++ src/main/scala/expr/Expr.scala
@@ -1,5 +1,7 @@
 package expr
 
+import spreadsheet.{Position, SpreadSheet}
+
 sealed trait Expr {
   def value: Long
 }
@@ -17,10 +19,27 @@ case class Add(a: Expr, b: Expr) extends Expr {
 }
 
 object Expr {
-  val ops = List('*', '+')
 
-  def fromString(str: String) = {
-    ???
+  import SpreadSheet._
+
+  def fromString(str: String)(implicit sh: SpreadSheet) = {
+    str.split(" ").toList.foldRight(List[Expr]())(
+      (symbol, list) => (symbol, list) match {
+        case ("+", x :: y :: rest) => Add(if (cellMatcher(x.value.toString)) cellSplitter(x.value.toString) else x, if (cellMatcher(y.value.toString)) cellSplitter(y.value.toString) else y) :: rest
+        case ("*", x :: y :: rest) => Mul(if (cellMatcher(x.value.toString)) cellSplitter(x.value.toString) else x, if (cellMatcher(y.value.toString)) cellSplitter(y.value.toString) else y) :: rest
+        case (_, _) => Constant(if (cellMatcher(symbol)) {
+          sh.eval(cellSplitter(symbol)).getOrElse(0L)
+        } else symbol.toLong) :: list
+      }).head
+  }
+
+  private def cellMatcher(maybeCell: String) = {
+    maybeCell.matches("[a-z][0-9]+")
+  }
+
+  private def cellSplitter(definitelyCell: String) = {
+    val alphaPart = definitelyCell.takeWhile(Character.isLetter(_))
+    Position(alphaPart(0), definitelyCell.drop(alphaPart.length).toInt)
   }
 }
 
--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -3,19 +3,50 @@ import spreadsheet._
 import org.scalatest._
 
 class ParserTest extends FlatSpec with Matchers {
+
+  def fixture = new {
+    val spreadSheet = new SpreadSheet()
+  }
+
+  val one: Expr = Constant(1)
+  val two: Expr = Constant(2)
+  val three: Expr = Constant(3)
+  val sum13: Expr = Add(three, one)
+  val mul23: Expr = Mul(two, three)
+
   "3 + 1" should "equal 4 when the expression is parsed from string" in {
-    /*  You might need to pass a spreadsheet as an argument to the parser, depending
-        how you handle cell references.
-     */
+    val fix = fixture
+    implicit val firstAddition = fix.spreadSheet
     val expr: Expr = Expr.fromString("+ 3 1")
     expr.value should be (4)
   }
 
   "3 + 1 * 4" should "equal 7 when the expression is parsed from string" in {
-    /*  You might need to pass a spreadsheet as an argument to the parser, depending
-        how you handle cell references.
-     */
+    val fix = fixture
+    implicit val firstAddition = fix.spreadSheet
     val expr: Expr = Expr.fromString("+ 3 * 1 4")
     expr.value should be (7)
+  }
+
+  "(3 + 1) * (4 + 3)" should "equal 28 when the expression is parsed from string" in {
+    val fix = fixture
+    implicit val firstAddition = fix.spreadSheet
+    val expr: Expr = Expr.fromString("* + 3 1 + 4 3")
+    expr.value should be (28)
+  }
+
+
+  "3 + 1 * a2" should "equal 6 when the expression is parsed from string and a2 set to 3" in {
+    val fix = fixture
+    implicit val firstAddition = fix.spreadSheet.add(Position('a', 2), three)
+    val expr: Expr = Expr.fromString("+ 3 * 1 a2")
+    expr.value should be (6)
+  }
+
+  "a1 + a2" should "equal 5 when the expression is parsed from string, both a2 and a1 set" in {
+    val fix = fixture
+    implicit val firstAddition = fix.spreadSheet.add(Position('a', 2), three).add(Position('a', 1), two)
+    val expr: Expr = Expr.fromString("+ a1 a2")
+    expr.value should be (5)
   }
 }
